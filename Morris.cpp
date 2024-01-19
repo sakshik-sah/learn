8cd795ad9e1f22eb268fdc9b6d897e1290f9a4a3
sakshik-sah <sahsakshi6533@gmail.com>
2024-01-19 23:17:01 +0530

    added message in Morris.cpp

--- Morris.cpp
+++ Morris.cpp
@@ -53,6 +53,7 @@ int main(){
     root->right = new treeNode(3);
     root->left->left = new treeNode(4);
     root->left->right = new treeNode(5);
+    cout<<"Added to git :"<<endl;
     cout<<"Morris Traversal result of provided binary tree is as follows -"<<endl;
     MorrisTraversal(root);
  
